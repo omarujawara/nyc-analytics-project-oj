6aadfa653697f0ff869372f56c00f449c89b5724
Omaru Jawara <jawaraomaru@yahoo.com>
2026-03-28 18:10:02 +0000

    Build dim_location.sql model

---
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -0,0 +1,31 @@
+-- Location dimension shared by both restaurant applications and 311 service reqs
+
+WITH all_locations AS (
+   -- Get locations from 311 requests
+   SELECT DISTINCT
+      CAST(borough AS STRING) AS borough,
+      CAST(incident_zip AS STRING) AS zip_code
+        --TODO replace (HINT: look @ dimensional model & staging data!)
+   FROM {{ ref('stg_nyc_311_dot') }}
+   WHERE borough IS NOT NULL
+
+   UNION DISTINCT
+
+   -- Get locations from restaurant applications
+   SELECT DISTINCT
+       CAST(borough AS STRING) AS borough,
+      CAST(zip AS STRING) AS zip_code
+       -- TODO replace (HINT: look @ dimensional model & staging data!)
+   FROM {{ ref('stg_nyc_open_restaurant_apps') }}
+   WHERE borough IS NOT NULL
+),
+
+location_dimension AS (
+   SELECT
+       {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
+       borough,
+       zip_code
+   FROM all_locations
+)
+
+SELECT * FROM location_dimension --TODO replace ??s with what to select. HINT: May be quite simple!
